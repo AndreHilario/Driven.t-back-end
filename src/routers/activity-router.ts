a73dda712c2744db2e82c9c3757df7910da7007f
MarianaKely <marianakely.r@gmail.com>
2023-08-08 23:35:29 -0300

    feat: activities routers

---
+++ src/routers/activity-router.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { activitySchema } from "@/schemas";
+import { allActivities , findByDate , subscribePerActivity } from "@/controllers";
+import { validateBody , authenticateToken } from "@/middlewares";
+
+
+
+const activitiesRouter = Router();
+
+activitiesRouter
+ .all('/*', authenticateToken)
+ .get('/', allActivities)
+ .get('/activitydate', findByDate)
+ .post('/', validateBody(activitySchema), subscribePerActivity);
+
+
+export {activitiesRouter}  
