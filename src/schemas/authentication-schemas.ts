309aa0ec54655a5d1042ecacc6f46a0ed932dd56
Andre Hilario <andre18hilario@gmail.com>
2023-08-11 18:31:37 -0300

    feat: login with Github

--- src/schemas/authentication-schemas.ts
+++ src/schemas/authentication-schemas.ts
@@ -1,7 +1,11 @@
-import { SignInParams } from "@/services";
+import { SignInParams, SignInParamsOAuth } from "@/services";
 import Joi from "joi";
 
 export const signInSchema = Joi.object<SignInParams>({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
+});
+
+export const signInGithubSchema = Joi.object<SignInParamsOAuth>({
+  githubCode: Joi.string().required(),
 });
